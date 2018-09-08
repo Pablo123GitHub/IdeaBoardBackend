875340e82e35553c0627c89ff18c712b74fc74d9
Pablo <paul.ladiv123@gmail.com>
2018-09-08 12:33:52 +0100

    add seed file to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ideas = Idea.create(
+                [
+                    {
+                        title: 'Create a reminder App',
+                        body: 'For appointments...etc.'
+                    },
+                    {
+                        title: 'Fitness App ',
+                        body: 'to Remember to exercise'
+                    },
+                    {
+                        title: 'eat well app ',
+                        body: 'prevent from eating only burgers'
+                    }
+                ]
+)
